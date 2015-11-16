44718eea37799e212e44ee7f059de0f03ed92edd
AustinScott3573 <Ascott3573@gmail.com>
2015-11-16 14:19:10 -0600

    Second commit, master unfortuantely. Messed up. added HTML file and additional CSS for a base. Will branch off next time.

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -1,14 +1,16 @@
 require.config({
-  baseUrl: './javascripts',
+  baseUrl: "./javascripts",
   paths: {
-    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
-    'lodash': '../lib/bower_components/lodash/lodash.min',
-    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'q': '../lib/bower_components/q/q',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "q": "../lib/bower_components/q/q"
   },
   shim: {
-    'bootstrap': ['jquery']
+    "bootstrap": ["jquery"],
+    "firebase": {exports: "Firebase"}
   }
 });
 
