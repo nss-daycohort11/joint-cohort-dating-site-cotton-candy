1cc1c1d074b303001df695ba4840e0ed423133c8
Danielle Hollis <hollisd09@gmail.com>
2015-11-16 11:43:32 -0600

    First changes, fixed files, added main.css

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -1,4 +1,18 @@
 require.config({
+  baseUrl: "./javascripts",
+  paths: {
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "q": "../lib/bower_components/q/q"
+  },
+  shim: {
+    "bootstrap": ["jquery"],
+    "firebase": {exports: "Firebase"}
+  }
+});require.config({
   baseUrl: './javascripts',
   paths: {
     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
